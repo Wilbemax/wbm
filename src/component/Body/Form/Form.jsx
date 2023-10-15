d516e4dbc86e3f320e8abf8bf5cf85b13ae79a29
Wilbemax <max22052007@yandex.ru>
2023-10-15 19:24:16 +0300

    main

--- src/component/Body/Form/Form.jsx
+++ src/component/Body/Form/Form.jsx
@@ -1,27 +1,47 @@
-import React, {  useRef } from "react";
+import React, { useRef } from "react";
 import styels from "./Form.module.css";
 import { FormItem } from "./FormItem/FormItem";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
+import { motion } from "framer-motion";
+import { offersCardAnimations } from "../../animations/animations";
 
 export const Form = () => {
-
-  
   const form = useRef();
 
   const sendEmail = (e) => {
+    console.log(e)
     e.preventDefault();
+    
 
-    emailjs.sendForm('service_q6sg8f8', 'template_j0n93nb', form.current, 'xSWgWwOwsnm9kxc3a')
-      .then((result) => {
+    emailjs
+      .sendForm(
+        "service_q6sg8f8",
+        "template_j0n93nb",
+        form.current,
+        "xSWgWwOwsnm9kxc3a"
+      )
+      .then(
+        (result) => {
           console.log(result.text);
-      }, (error) => {
+          e.target.reset()
+          alert("Обращение успешно отправленно ")
+        },
+        (error) => {
           console.log(error.text);
-      });
+          alert("произошла какая-то ошибка по пробуйте позже")
+        }
+      );
   };
 
-
   return (
-    <div className={styels.Form}>
+    <motion.div
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ amount: 0.3, once: true }}
+      variants={offersCardAnimations}
+      transition={{ duration: 0.7 }}
+      className={styels.Form}
+    >
       <div className={styels.wrapper}>
         <div className={styels.textBlock}>
           <p className={styels.title}>Обратитесь к нам за консультацией</p>
@@ -44,7 +64,7 @@ export const Form = () => {
               <div className={styels.inputWrapper}>
                 <p className={styels.formTitle}>Имя</p>
                 <input
-                name="user_name"
+                  name="user_name"
                   className={styels.input}
                   type="text"
                   placeholder="Введите ваше имя"
@@ -53,7 +73,7 @@ export const Form = () => {
               <div className={styels.inputWrapper}>
                 <p className={styels.formTitle}>Фамилия</p>
                 <input
-                name="user_seconName"
+                  name="user_seconName"
                   className={styels.input}
                   type="text"
                   placeholder="Введите вашу фамилию"
@@ -64,7 +84,7 @@ export const Form = () => {
               <div className={styels.inputWrapper}>
                 <p className={styels.formTitle}>Почта</p>
                 <input
-                name="user_email"
+                  name="user_email"
                   className={styels.input}
                   type="email"
                   placeholder="Введите вашу почту"
@@ -73,22 +93,30 @@ export const Form = () => {
               <div className={styels.inputWrapper}>
                 <p className={styels.formTitle}>Tелефон</p>
                 <input
-                name="user_nomber"
+                  name="user_nomber"
                   className={styels.input}
-                  type="text"
                   
+                  type="text"
                   placeholder="Введите номер телефона"
                 />
               </div>
             </div>
             <div className={styels.wrapTextArea}>
               <p className={styels.formTitle}>Как мы можем вам помочь?</p>
-              <textarea className={styels.textArea} name="message" id="" cols="30" rows="10"></textarea>
+              <textarea
+                className={styels.textArea}
+                name="message"
+                id=""
+                cols="30"
+                rows="10"
+              ></textarea>
             </div>
-            <button className={styels.button} type="submit">Отрправить заявку</button>
+            <button className={styels.button} type="submit">
+              Отрправить заявку
+            </button>
           </div>
         </form>
       </div>
-    </div>
+    </motion.div>
   );
 };
