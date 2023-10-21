77bf7305c20889d04fc0e065ca7316280b00235c
Wilbemax <max22052007@yandex.ru>
2023-10-21 21:11:58 +0300

    main

--- src/component/Header/Nav/Nav.jsx
+++ src/component/Header/Nav/Nav.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styels from "./Nav.module.css";
+import logo from "../../../assets/1213.png"
  
 
 export const Nav = () => {
@@ -8,7 +9,7 @@ export const Nav = () => {
       <div className={styels.wrapper}>
         <div className={styels.logo}>
           {" "}
-          WBM<p className={styels.logo_dot}>.company</p>
+          <img className={styels.logos} src={logo} alt="WBM.company" />
         </div>
         <div className={styels.link_wrap}>
           <a className={styels.link} href="/#header">
