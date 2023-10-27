5dd6ab1dbcd4a0f75ef78a00633b073e0331d352
Wilbemax <max22052007@yandex.ru>
2023-10-27 18:16:22 +0300

    main

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import styels from "./Footer.module.css";
 import { HiOutlineMail } from "react-icons/hi";
 import { FaTelegramPlane, FaWhatsapp } from "react-icons/fa";
+import logo from "../../assets/1213.png"
 
 export const Footer = () => {
   return (
     <div className={styels.Footer} id="contact">
       <div className={styels.wrapper}>
         <div className={styels.logo}>
-          <p>WBM.company</p>
+          <img src={logo} alt="WBM.company" />
         </div>
         <div className={styels.contact}>
           <a className={styels.a}href="mailto:wbm.company@mail.ru" rel="noreferrer" target="_blank">
             <HiOutlineMail className={styels.icon}/>
             wbm.company@mail.ru
           </a>
-          <a
+          <a className={styels.a}
             href="https://web.telegram.org/k/#@jkdlon"
             rel="noreferrer"
             target="_blank"
@@ -23,7 +24,7 @@ export const Footer = () => {
             <FaTelegramPlane className={styels.icon}/>
             Telegram
           </a>
-          <a
+          <a className={styels.a}
             href="https://api.whatsapp.com/send/?phone=79219292776&text=Здравствуйте%2C+у+меня+есть+вопрос"
             rel="noreferrer"
             target="_blank"
